a14e2d28ccafdcefe057807764c2ebe6993fb50c
nakagomi51 <nakagomi@hotmail.com>
2021-10-02 07:43:24 +0900

    Create test01.py

---
+++ test01.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import bme280_custom
+import time
+from datetime import datetime
+
+try:
+	while True:
+
+		#現在の時間を取得
+		pub_time = datetime.now().strftime("%H:%M:%S")
+
+		#センサーから温度、湿度、気圧を取得
+		pub_temp = bme280_custom.compensate_T()
+		pub_pres = bme280_custom.compensate_P()
+		pub_humi = bme280_custom.compensate_H()
+
+		#画面に情報表示
+		print ("Time:" ,pub_time)	#時間
+		print ("Temp:", pub_temp)	#温度
+		print ("Humi:", pub_humi)	#湿度
+		print ("Pres:", pub_pres)	#気圧
+		print ("-------------------------")
+
+		#スリープ
+		time.sleep(3.0)
+
+except KeyboardInterrupt:
+	pass
